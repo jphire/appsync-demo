0799b9a4701cc539e9041cf23852dd093151ad61
Janne Laukkanen <janne.laukkanen83@gmail.com>
2019-05-29 10:25:14 +0300

    Clean obsolete stuff, make meters responsive

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import GaugeChart from 'react-gauge-chart';
 import ReactSpeedometer from "react-d3-speedometer"
-import logo from './logo.gif';
 import logoBeckhoff from './beckhoff.jpg';
 import poplatekLogo from './poplatek-logo.svg'
 import Amplify, { API, graphqlOperation } from "aws-amplify";
@@ -12,13 +10,8 @@ import * as subscriptions from './graphql/subscriptions';
 import * as queries from './graphql/queries';
 
 import {
-  Collapse,
   Navbar,
-  NavbarToggler,
   NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
   Col,
   Row
 } from 'reactstrap';
@@ -60,25 +53,22 @@ class App extends React.Component {
           <NavbarBrand href="#">
             <img src={logoBeckhoff} width={250} className="beckhoff" alt="logo" />
           </NavbarBrand>
-          {/* <NavbarToggler /> */}
           <NavbarBrand href="#" className="ml-auto">          
             <img src={poplatekLogo} className="poplatekLogo" alt="logo2" />
           </NavbarBrand>
         </Navbar>
 
         <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <Col className='col-12'>
             <Row>
-              <Col className='col-6'>
-                {/* <GaugeChart id="gauge-chart2" nrOfLevels={20} percent={0.86} /> */}
+              <Col className='col-6 col-md-6 col-sm-12'>
                 <ReactSpeedometer 
-                  value={this.state.data.voltage} minValue={0} maxValue={200} width={600} height={350} needleHeightRatio={0.7}/>
-                <h4>Voltage</h4>
+                  value={this.state.data.voltage} segments={3} startColor={'#ff0000'} endColor={'#33cc33'} minValue={0} maxValue={200} width={600} height={350} needleHeightRatio={0.7}/>
+                <h4>Voltage (V)</h4>
               </Col>
-              <Col className='col-6'>
-                <ReactSpeedometer value={this.state.data.current} minValue={0} maxValue={15} width={600} height={350} needleHeightRatio={0.7}/>
-                <h4>Current</h4>
+              <Col className='col-6 col-md-6 col-sm-12'>
+                <ReactSpeedometer value={this.state.data.current} startColor={'#33cc33'} endColor={'#ff0000'} minValue={0} maxValue={15} width={600} height={350} needleHeightRatio={0.7}/>
+                <h4>Current (A)</h4>
               </Col>
             </Row>
           </Col>
